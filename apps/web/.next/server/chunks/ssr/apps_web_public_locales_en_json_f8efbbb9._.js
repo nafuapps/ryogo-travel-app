031bd659dabdaca777f267a4b217144ef3a5d182
kakashiorion <nitin.kool.1808@gmail.com>
2025-09-12 19:07:03 +0530

    Created Agency app

---
+++ apps/web/.next/server/chunks/ssr/apps_web_public_locales_en_json_f8efbbb9._.js
@@ -0,0 +1,5 @@
+module.exports = [
+"[project]/apps/web/public/locales/en.json (json)", ((__turbopack_context__) => {
+
+__turbopack_context__.v(JSON.parse("{\"Landing\":{\"title\":\"Hello world!\"}}"));}),
+];

115f1ca638c24b89d7dbc567586a81cf3d0d38ac
kakashiorion <nitin.kool.1808@gmail.com>
2026-01-26 09:01:35 +0530

    fixed allDriverLeaves prop error

--- apps/web/src/app/dashboard/drivers/[id]/leaves/page.tsx
+++ apps/web/src/app/dashboard/drivers/[id]/leaves/page.tsx
@@ -4,6 +4,9 @@ import DashboardHeader from "@/app/dashboard/components/extra/dashboardHeader"
 import { driverServices } from "@ryogo-travel-app/api/services/driver.services"
 import AllDriverLeavesPageComponent from "./allDriverLeaves"
 import { mainClassName } from "@/components/page/pageCommons"
+import { getCurrentUser } from "@/lib/auth"
+import { redirect, RedirectType } from "next/navigation"
+import { UserRolesEnum } from "@ryogo-travel-app/db/schema"
 
 export default async function AllDriverLeavesPage({
   params,
@@ -11,13 +14,21 @@ export default async function AllDriverLeavesPage({
   params: Promise<{ id: string }>
 }) {
   const { id } = await params
-
+  const currentUser = await getCurrentUser()
+  if (!currentUser) {
+    redirect("/auth/login", RedirectType.replace)
+  }
   const driverLeaves = await driverServices.findAllDriverLeavesByDriverId(id)
 
   return (
     <div className={mainClassName}>
       <DashboardHeader pathName={"/dashboard/drivers/[id]/leaves"} />
-      <AllDriverLeavesPageComponent leaves={driverLeaves} driverId={id} />
+      <AllDriverLeavesPageComponent
+        leaves={driverLeaves}
+        driverId={id}
+        userId={currentUser.userId}
+        isOwner={currentUser.userRole == UserRolesEnum.OWNER}
+      />
     </div>
   )
 }
